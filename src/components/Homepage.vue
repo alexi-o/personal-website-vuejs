0813647e04df73d77c97684e90986ac86ac2aa1a
Alexi OHearn <ohearnalexi@gmail.com>
2020-09-16 17:39:08 -0600

    adds scss structure

---
+++ src/components/Homepage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>banana</h2>
+  </div>
+</template>
+<script>
+export default {
+    name: 'Homepage',
+    props: {
+        msg: String
+    }
+}
+</script>
+<style scoped>
+</style>
